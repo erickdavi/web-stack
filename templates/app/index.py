39bb86869fc32f431631f98cb50f9cf8c5ed32a0
erickdavi@gmail.com <erickdavi@gmail.com>
2019-01-03 04:50:38 +0000

    Correcao do apache wsgi

--- templates/app/index.py
+++ templates/app/index.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
- 
+
 @app.route("/")
 def hello():
-return "Hello World!"
- 
+    return "Hey Dude!!"
+
 if __name__ == "__main__":
-app.run()
+    app.run()
---
+++ templates/app/wsgi.py
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0, "/var/www/app")
+from index import app as application
